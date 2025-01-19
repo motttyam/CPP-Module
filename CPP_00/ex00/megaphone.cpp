432f2ee3e326d93809ceea0bcc8bd2bb108ccb3d
Kazuki Tsukamoto <ktsukamo@c1r1s1.42tokyo.jp>
2025-01-19 13:48:56 +0900

    fixed Makefile : add -std=c++98

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 
     if (argc == 1){
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-        return (EXIT_SUCCESS);
+        return (0);
     }
     
     for (int i = 1; i < argc;) {
@@ -19,5 +19,5 @@ int main(int argc, char *argv[])
         char c = toupper(arg[i]);
         std::cout << c;
     }
-    return (EXIT_SUCCESS);
+    return (0);
 }
