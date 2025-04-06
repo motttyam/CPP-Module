4900b26114c75d9e3d15e70a9271cf9e9e86134d
Kazuki Tsukamoto <ktsukamo@c6r2s11.42tokyo.jp>
2025-04-06 16:04:33 +0900

    [Fix] small fix CPP-02 ex03

--- CPP_02/ex03/Point.cpp
+++ CPP_02/ex03/Point.cpp
@@ -1,12 +1,12 @@
 #include "Point.hpp"
 
-Point::Point() : _x(0), _y(0) {};
+Point::Point() : _x(0), _y(0) {}
 
-Point::Point(Fixed const &x, Fixed const &y) : _x(x), _y(y) {};
+Point::Point(Fixed const &x, Fixed const &y) : _x(x), _y(y) {}
 
-Point::Point(Point const &value) : _x(value._x), _y(value._y) {};
+Point::Point(Point const &value) : _x(value._x), _y(value._y) {}
 
-Point::~Point(){};
+Point::~Point(){}
 
 // private member func
 Fixed const& Point::getX() const {
